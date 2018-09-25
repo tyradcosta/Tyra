89fc666cd309ee0cc85a6fc164c04deeaae5107f
Tyra <tyradcosta22@gmail.com>
2018-09-25 13:44:03 -0400

    snake

--- Atelier/sketch1/sketch.js
+++ Atelier/sketch1/sketch.js
@@ -1,38 +1,123 @@
-var angle = 0;
- var speed = 0.05;
-var centerX = 300;
-var centerY = 300; 
-var radius = 150;
-
+var snake;
+var rez = 20;
+var food;
+var w;
+var h;
+var munch;
+var points= 0;
+var level = 1;
+var button; 
 
 
 function setup() {
-  createCanvas(innerWidth,innerHeight);
+  createCanvas(windowWidth,windowHeight);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(7);
+  snake = new Snake();
+ kity = loadImage("assets/kity.png");
     
+  foodLocation();
+
+  soundFormats('mp3', 'ogg');
+  munch = loadSound('assets/munch.mp3');
+    
+   var points = 0; 
+    
+    button = createButton("Click to Start");
+    button.position(0,0);
+    button.mousePressed(startGame);
+    
+}
+
+function foodLocation() {
+  var x = floor(random(w));
+  var y = floor(random(h));
+  food = createVector(x, y);
+  
+}
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
 }
 
 function draw() {
-    
-    background(0,10)
 
+     background("#F4E5CB");
+    border();
+    startGame();
+
+function levelup(){
     
-var x = mouseX + mouseX/2 * cos (angle);
-    
- var y = mouseY + mouseY/2 * sin (angle);
+scale(1);
+
+fill("#B2C8D1");
+rect(0,0,12,2);
+
+    fill("white");
+textSize(1);
+text("Level " + level, 2, 1.5);
+text("Points" + points,6, 1.5);
 
 
-    
-  noStroke();
-    fill(mouseX,mouseY,80);
-ellipse(x,y,75,75);
-    
-   noStroke();
-     fill(mouseX,mouseY,150);
-    ellipse(x,y,40,75);
-      
-    for(var i = 0; i<5; i++){
-  i += angle;
-         angle = angle+speed;  
+
 }
+
+
+function border() {
     
+  stroke(255);
+  rect(0,0,width,height);
+}
+
+
+
+function startGame(){
+    
+    
+  scale(rez);
+  if (snake.eat(food)) {
+     foodLocation();
+      points+= 10 ;
+        print(points);
+       munch.play();  
+
+  }
+  snake.update();
+  snake.show();
+   noStroke();
+	
+//fill("#F476D1");
+
+image(kity,food.x,food.y,3);
+    
+  if (snake.endGame()) {
+     
+    print("END GAME");
+    background("#B6A895");
+       noLoop();
+      textSize(3);
+      fill("#FFB5C2");
+      
+      textAlign(CENTER);	
+text('GAME OVER',18,10);
+
+  }
+    
+    levelup();
+}
+
+
+
 }
---
+++ Atelier/sketch1/snake.js
@@ -0,0 +1,70 @@
+class Snake {
+    
+  
+  constructor() {
+  	this.body = [];
+    this.body[0] = createVector(floor(w/2), floor(h/2));
+    this.xdir = 0;
+    this.ydir = 0;
+    this.len = 0;
+  }
+  
+  setDir(x, y) {
+  	this.xdir = x;
+    this.ydir = y;
+  }
+  
+  update() {
+  	var head = this.body[this.body.length-1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+  
+  grow() {
+  	var head = this.body[this.body.length-1].copy();
+    //this.len++;
+     // console.log(this.body.length);
+    this.body.push(head);
+    //  console.log(this.body.length);
+  }
+  
+  endGame() {
+  	var x = this.body[this.body.length-1].x;
+    var y = this.body[this.body.length-1].y;
+    if(x > w || x < 0 || y > h || y < 0) {
+       return true;
+    }
+    for(var i = 0; i < this.body.length-1; i++) {
+    	var part = this.body[i];
+      if(part.x === x && part.y === y) {
+      	return true;
+      }
+    }
+    return false;
+  }
+  
+  eat(pos) {
+  	var x = this.body[this.body.length-1].x;
+    var y = this.body[this.body.length-1].y;
+		var eat = dist(x,y,food.x,food.y);
+    if(eat <= 3) {
+      this.grow();
+      return true;
+        munch.play();   
+        points+= 1 ;
+        print(points);
+    }
+    return false;
+  }
+  
+  show() {
+  	for(var i = 0; i < this.body.length; i++) {
+        noStroke();
+    	fill("#ffb5c2");
+      ellipse(this.body[i].x, this.body[i].y,3)
+    }
+  }
+
+}
